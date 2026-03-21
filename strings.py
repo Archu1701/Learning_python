b868084870e3e818f94504d86c893c073f994223
Archana <archanadevip17@gmail.com>
2026-03-21 20:34:37 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,25 @@
+name="Archana"
+subname="Devi"
+length=len(name)
+con= name + subname
+print("The length of the  name",length)
+print("The length of the  subname",len(subname))
+print("concatenate",con)
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      )
